6a3a2dd246cd55e43988d83b4e5b7f07060791d0
Felix Schulze <code@felixschulze.de>
2011-02-01 13:03:12 +0100

    GnuTLS 2.8.6 with example xcode project

---
+++ Classes/GnuTLS_for_iOSAppDelegate.m
@@ -0,0 +1,126 @@
+//
+//  GnuTLS_for_iOSAppDelegate.m
+//  GnuTLS-for-iOS
+//
+//  Created by Felix Schulze on 01.02.11.
+//  Copyright 2010 Felix Schulze. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+
+#import "GnuTLS_for_iOSAppDelegate.h"
+#include <gnutls/openssl.h>
+
+@implementation GnuTLS_for_iOSAppDelegate
+
+@synthesize window, textField, md5TextField;
+
+#pragma mark -
+#pragma mark OpenSSL
+
+- (IBAction)calculateMD5:(id)sender
+{
+	/** Calculate MD5*/
+	NSString *string =  textField.text;
+    unsigned char *inStrg = (unsigned char*)[[string dataUsingEncoding:NSASCIIStringEncoding] bytes];
+    unsigned long lngth = [string length];
+	unsigned char result[MD5_DIGEST_LENGTH];
+	NSMutableString *outStrg = [NSMutableString string];
+	
+    MD5(inStrg, lngth, result);
+	
+    unsigned int i;
+    for (i = 0; i < MD5_DIGEST_LENGTH; i++)
+    {
+        [outStrg appendFormat:@"%02x", result[i]];
+    }
+	md5TextField.text = outStrg;
+	
+	//Hide Keyboard after calculation
+	[textField resignFirstResponder];
+}
+
+#pragma mark -
+#pragma mark Application lifecycle
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
+    [self.window makeKeyAndVisible];
+    
+    return YES;
+}
+
+- (IBAction)showInfo {
+	
+	UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"GnuTLS-for-iOS" message:@"GnuTLS-Version: 2.8.6\nlibgcrypt-Version: 1.4.6\nlibgpg-error-Version: 1.10\n\nLicense: See include/*/LICENSE\n\nCopyright 2011 by Felix Schulze\n http://www.x2on.de" delegate:nil cancelButtonTitle:@"Close" otherButtonTitles:nil];
+	
+	[alert show];
+	[alert release];
+}
+
+
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    /*
+     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+     */
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    /*
+     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+     If your application supports background execution, called instead of applicationWillTerminate: when the user quits.
+     */
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    /*
+     Called as part of  transition from the background to the inactive state: here you can undo many of the changes made on entering the background.
+     */
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    /*
+     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+     */
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    /*
+     Called when the application is about to terminate.
+     See also applicationDidEnterBackground:.
+     */
+}
+
+
+#pragma mark -
+#pragma mark Memory management
+
+- (void)applicationDidReceiveMemoryWarning:(UIApplication *)application {
+    /*
+     Free up as much memory as possible by purging cached data objects that can be recreated (or reloaded from disk) later.
+     */
+}
+
+
+- (void)dealloc {
+    [window release];
+    [super dealloc];
+}
+
+
+@end
---
+++ build-libgnutls.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+#  Automatic build script for gnutls
+#  for iPhoneOS and iPhoneSimulator
+#
+#  Created by Felix Schulze on 30.01.11.
+#  Copyright 2010 Felix Schulze. All rights reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+###########################################################################
+#  Change values here
+#
+VERSION="2.8.6"
+SDKVERSION="4.2"
+#
+###########################################################################
+#
+# Don't change anything here
+CURRENTPATH=`pwd`
+ARCHS="i386 armv6 armv7"
+
+
+##########
+set -e
+if [ ! -e gnutls-${VERSION}.tar.bz2 ]; then
+	echo "Downloading gnutls-${VERSION}.tar.bz2"
+    curl -O ftp://ftp.gnu.org/gnu/gnutls/gnutls-${VERSION}.tar.bz2
+else
+	echo "Using gnutls-${VERSION}.tar.bz2"
+fi
+
+if [ -f ${CURRENTPATH}/bin/iPhoneSimulator${SDKVERSION}-i386.sdk/lib/libgcrypt.a ];
+then 
+  echo "Using libgcrypt"
+else
+  echo "Please build libgcrypt first"
+  exit 1
+fi
+
+mkdir -p bin
+mkdir -p lib
+mkdir -p src
+
+for ARCH in ${ARCHS}
+do
+	if [ "${ARCH}" == "i386" ];
+	then
+		PLATFORM="iPhoneSimulator"
+	else
+		PLATFORM="iPhoneOS"
+	fi
+
+	tar zxvf gnutls-${VERSION}.tar.bz2 -C src
+	cd src/gnutls-${VERSION}
+	
+	echo "Building gnutls for ${PLATFORM} ${SDKVERSION} ${ARCH}"
+	echo "Please stand by..."
+	
+	sed -ie "s!gnutls_log_func _gnutls_log_func;!gnutls_log_func _gnutls_log_func = NULL;!" "lib/gnutls_global.c"
+
+
+
+	export DEVROOT="/Developer/Platforms/${PLATFORM}.platform/Developer"
+	export SDKROOT="${DEVROOT}/SDKs/${PLATFORM}${SDKVERSION}.sdk"
+	export CC=${DEVROOT}/usr/bin/gcc
+	export LD=${DEVROOT}/usr/bin/ld
+	export CPP=${DEVROOT}/usr/bin/cpp
+	export CXX=${DEVROOT}/usr/bin/g++
+	export AR=${DEVROOT}/usr/bin/ar
+	export AS=${DEVROOT}/usr/bin/as
+	export NM=${DEVROOT}/usr/bin/nm
+	export CXXCPP=$DEVROOT/usr/bin/cpp
+	export RANLIB=$DEVROOT/usr/bin/ranlib
+	export LDFLAGS="-arch ${ARCH} -pipe -no-cpp-precomp -isysroot ${SDKROOT} -L${CURRENTPATH}/lib"
+	export CFLAGS="-arch ${ARCH} -pipe -no-cpp-precomp -isysroot ${SDKROOT} -I${CURRENTPATH}/include"
+	export CXXFLAGS="-arch ${ARCH} -pipe -no-cpp-precomp -isysroot ${SDKROOT} -I${CURRENTPATH}/include"
+
+	mkdir -p "${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk"
+
+	LOG="${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk/build-gnutls-${VERSION}.log"
+
+	./configure --host=${ARCH}-apple-darwin --prefix="${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk" --enable-shared=no --with-libgcrypt-prefix="${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk" >> "${LOG}" 2>&1
+
+	make >> "${LOG}" 2>&1
+	make install >> "${LOG}" 2>&1
+	cd ${CURRENTPATH}
+	rm -rf src/gnutls-${VERSION}
+	
+done
+
+echo "Build library..."
+lipo -create ${CURRENTPATH}/bin/iPhoneSimulator${SDKVERSION}-i386.sdk/lib/libgnutls.a ${CURRENTPATH}/bin/iPhoneOS${SDKVERSION}-armv6.sdk/lib/libgnutls.a ${CURRENTPATH}/bin/iPhoneOS${SDKVERSION}-armv7.sdk/lib/libgnutls.a -output ${CURRENTPATH}/lib/libgnutls.a
+lipo -create ${CURRENTPATH}/bin/iPhoneSimulator${SDKVERSION}-i386.sdk/lib/libgnutls-extra.a ${CURRENTPATH}/bin/iPhoneOS${SDKVERSION}-armv6.sdk/lib/libgnutls-extra.a ${CURRENTPATH}/bin/iPhoneOS${SDKVERSION}-armv7.sdk/lib/libgnutls-extra.a -output ${CURRENTPATH}/lib/libgnutls-extra.a
+lipo -create ${CURRENTPATH}/bin/iPhoneSimulator${SDKVERSION}-i386.sdk/lib/libgnutls-openssl.a ${CURRENTPATH}/bin/iPhoneOS${SDKVERSION}-armv6.sdk/lib/libgnutls-openssl.a ${CURRENTPATH}/bin/iPhoneOS${SDKVERSION}-armv7.sdk/lib/libgnutls-openssl.a -output ${CURRENTPATH}/lib/libgnutls-openssl.a
+lipo -create ${CURRENTPATH}/bin/iPhoneSimulator${SDKVERSION}-i386.sdk/lib/libgnutlsxx.a ${CURRENTPATH}/bin/iPhoneOS${SDKVERSION}-armv6.sdk/lib/libgnutlsxx.a ${CURRENTPATH}/bin/iPhoneOS${SDKVERSION}-armv7.sdk/lib/libgnutlsxx.a -output ${CURRENTPATH}/lib/libgnutlsxx.a
+
+cp -R ${CURRENTPATH}/bin/iPhoneSimulator${SDKVERSION}-i386.sdk/include/gnutls ${CURRENTPATH}/include/
+echo "Building done."
